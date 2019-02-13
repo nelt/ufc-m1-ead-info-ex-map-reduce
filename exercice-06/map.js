7ab45bf62221e917dad00b72ac2f274ceec8f7b8
nelt <nel@flexio.fr>
2019-02-13 07:34:22 +0100

    exercice 6

---
+++ exercice-06/map.js
@@ -0,0 +1,31 @@
+/**
+statistiques par département :
+- le nombre de d’échantillon
+- température moyenne
+- température minimale
+- température maximale
+
++ pour les heures les plus chaudes (entre 13h et 15h) et les heures plus froides (entre 3h et 6 h)
++ par département et par mois
+
+*/
+function map(value) {
+    var stat = {
+       avg: value.montant,
+       cnt: 1,
+       min: value.montant,
+       max: value.montant
+   };
+
+   emit('stats - ' + value.departement, stat);
+   emit('stats - ' + value.departement + ' - ' + dateFormat(value.date, 'yyyy-MM'), stat);
+
+   if(dateHour(value.date) >= 13 && dateHour(value.date) < 15) {
+        emit('stats - heures chaudes - ' + value.departement, stat);
+        emit('stats - heures chaudes - ' + value.departement + ' - ' + dateFormat(value.date, 'yyyy-MM'), stat);
+   }
+   if(dateHour(value.date) >= 3 && dateHour(value.date) < 6) {
+        emit('stats - heures froides - ' + value.departement, stat);
+        emit('stats - heures froides - ' + value.departement + ' - ' + dateFormat(value.date, 'yyyy-MM'), stat);
+   }
+}
