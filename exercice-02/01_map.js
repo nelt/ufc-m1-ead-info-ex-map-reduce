d0f984ee2cccbd1801939f06a5b541eb8593f205
nelt <nel@flexio.fr>
2019-02-15 22:13:32 +0100

    supression d'emit inutiles

--- exercice-02/01_map.js
+++ exercice-02/01_map.js
@@ -1,7 +1,5 @@
 function map(value) {
-    emit('nombre de documents publiés', {cnt: 1});
     if(dateYear(value.published) == 2018 && dateMonth(value.published) == 12) {
         emit('publiés en décembre 2018', {cnt: 1});
     }
-    emit('' + dateFormat(value.published, 'yyyy-MM'), {cnt: 1});
 }
