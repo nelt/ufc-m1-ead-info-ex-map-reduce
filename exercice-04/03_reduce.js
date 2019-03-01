2e1195f21b90dab329d5024474d4886d7d2da698
G3rb <93rb1n4t0r@gmail.com>
2019-03-01 15:38:54 +0100

    Update 03_reduce.js
    Pour avoir le nombre d'éléments correct ce compteur doit être initialisé à 0 non ?

--- exercice-04/03_reduce.js
+++ exercice-04/03_reduce.js
@@ -1,5 +1,5 @@
 function reduce(values) {
-    var result = {ca: 0, cnt: 1};
+    var result = {ca: 0, cnt: 0};
     for each(var value in values) {
         result.ca += value.ca;
         result.cnt += value.cnt;
