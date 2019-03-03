a48de2dcff29b7302b1edf6a724ce41a37277759
nelt <nel@flexio.fr>
2019-03-03 14:20:38 +0100

    exercice 06 / redice : initialisation du minimum à null plutôt que -1 (une température peut être négative !)

--- exercice-06/reduce.js
+++ exercice-06/reduce.js
@@ -1,12 +1,12 @@
 function reduce(values) {
-    var result = {avg: 0, cnt: 0, min: -1, max: 0};
+    var result = {avg: 0, cnt: 0, min: null, max: 0};
     for each(var value in values) {
         result.avg = (result.avg * result.cnt + value.avg * value.cnt) / (result.cnt + value.cnt);
         result.cnt += value.cnt;
         if(result.max < value.max) {
             result.max = value.max;
         }
-        if(result.min == -1 || result.min > value.min) {
+        if(result.min == null || result.min > value.min) {
             result.min = value.min;
         }
     }
