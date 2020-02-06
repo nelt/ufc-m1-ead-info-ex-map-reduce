8148be551b63a7289efbdcbfab0a9b8a2c59c1ed
nelt <nel@flexio.fr>
2020-02-06 07:33:39 +0100

    mr.sh to add version swith for jdk 11 and higher to remove nashorn deprecation warning

--- mr.sh
+++ mr.sh
@@ -9,4 +9,14 @@ if [ ! -f "$JAR" ]; then
     exit 1
 fi
 
-java -jar "$JAR" "$@"
+# force jdk 11 on ubuntu 
+#JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/bin/
+#JAVA=/usr/lib/jvm/java-11-openjdk-amd64/bin/java
+JAVA=java
+
+JAVA_VERSION=$($JAVA -version 2>&1 | grep -i version | sed 's/.*version "\([0-9]*\)\.\([^"]*\)".*/\1/; 1q')
+JAVA_OPTS=
+if [[ "$JAVA_VERSION" -ge "11" ]]; then
+   JAVA_OPTS="-Dnashorn.args=--no-deprecation-warning"
+fi
+$JAVA $JAVA_OPTS -jar "$JAR" "$@"
