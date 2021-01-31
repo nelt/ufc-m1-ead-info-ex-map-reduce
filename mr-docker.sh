c37bfab895ff4fd91a217d399bb5c02d0b2adf80
nelt <nel@flexio.fr>
2021-01-31 21:37:45 +0100

    replaced java invocation with docker

--- mr-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm -it -v $PWD:/work  codingmatters/mr-simulation-app:1.0.2 mr "$@"
--- mr.sh
+++ mr.sh
@@ -1,22 +1,3 @@
 #!/usr/bin/env bash
 
-SCRIPT="$(readlink --canonicalize-existing "$0")"
-SCRIPTPATH="$(dirname "$SCRIPT")"
-
-JAR="$SCRIPTPATH/mr.jar"
-if [ ! -f "$JAR" ]; then
-    echo "$JAR non trouvé, pour exécuter les corrigés, télécharger au préalable la dernière release sur https://github.com/nelt/mr-simulation/releases"
-    exit 1
-fi
-
-# force jdk 11 on ubuntu 
-#JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/bin/
-#JAVA=/usr/lib/jvm/java-11-openjdk-amd64/bin/java
-JAVA=java
-
-JAVA_VERSION=$($JAVA -version 2>&1 | grep -i version | sed 's/.*version "\([0-9]*\)\.\([^"]*\)".*/\1/; 1q')
-JAVA_OPTS=
-if [[ "$JAVA_VERSION" -ge "11" ]]; then
-   JAVA_OPTS="-Dnashorn.args=--no-deprecation-warning"
-fi
-$JAVA $JAVA_OPTS -jar "$JAR" "$@"
+docker run --rm -it -v $PWD:/work  codingmatters/mr-simulation-app:1.0.2 mr "$@"
