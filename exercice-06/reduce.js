0d4af2898612f43e79c7c586c94d7f130f5534c8
David Prévot <david@tilapin.org>
2021-02-11 20:57:43 -0400

    Max temp can be negative

--- exercice-06/reduce.js
+++ exercice-06/reduce.js
@@ -1,9 +1,9 @@
 function reduce(values) {
-    var result = {avg: 0, cnt: 0, min: null, max: 0};
+    var result = {avg: 0, cnt: 0, min: null, max: null};
     for each(var value in values) {
         result.avg = (result.avg * result.cnt + value.avg * value.cnt) / (result.cnt + value.cnt);
         result.cnt += value.cnt;
-        if(result.max < value.max) {
+        if(result.max == null || result.max < value.max) {
             result.max = value.max;
         }
         if(result.min == null || result.min > value.min) {
